023f5118f89bee09cf1ae43ea3e1220935ac3be9
EliBloom <rollinggoron@gmail.com>
2022-05-16 01:22:29 -0400

    fixed simple test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "../components/App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Search Image...");
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText("Search Image...");
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("Basic Calculator Test", () => {
+  delete window.matchMedia;
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  });
+
+  describe("Calculator UI Functionality", () => {
+    test("The buttons of the Basic Calculator should be rendered", () => {
+      render(<App />);
+      const input = screen.getByText("Search Image...");
+      expect(input).toBeInTheDocument();
+    });
+  });
 });
